213f8484bed8041c21e2e158dc11d0646abe6d67
Song <schen@jerseymikes.com>
2022-08-04 16:54:33 -0400

    kind of working

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@
           {{ account }}
         </div>
       </button>
-
       <p>Enter a ENS Name</p>
       <div class="operation">
         <input v-model="name" />
@@ -18,48 +17,42 @@
           Search
         </button>
       </div>
-      <div v-if="isAvailable">
-        <div class="alert alert-primary" role="alert">
-          <b>{{ name }}.eth</b> is available!
+      <div v-if="searched">
+        <div v-if="isAvailable">
+          <div class="alert alert-primary" role="alert">
+            <b>{{ name }}.eth</b> is available!
+          </div>
+          <div class="card">
+            <div class="card-header">
+              <div class="input-group flex-nowrap">
+                <input
+                  type="number"
+                  class="form-control"
+                  value="30"
+                  @input="getInput($event)"
+                />
+                <span class="input-group-text">DAYS</span>
+              </div>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">{{ name }}.eth</h5>
+              <p class="card-text">{{ price }} ETH for {{ days }} Days</p>
+              <LoadingButton
+                :register="register"
+                :commit="commit"
+                :isLoading="isLoading"
+                :committed="committed"
+                :registered="registered"
+                :name="name"
+              ></LoadingButton>
+            </div>
+          </div>
         </div>
-        <div class="card">
-          <div class="card-header">
-            <div class="input-group flex-nowrap">
-              <input
-                type="number"
-                class="form-control"
-                value="30"
-                @input="getInput($event)"
-              />
-              <span class="input-group-text">DAYS</span>
-            </div>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">{{ name }}.eth</h5>
-            <p class="card-text">{{ price }} ETH for {{ days }} Days</p>
-            <div v-if="!wait">
-              <button type="button" @click="commit()" class="btn btn-primary">
-                Commit
-              </button>
-            </div>
-            <div v-else-if="wait && !completed">
-              <button class="btn btn-primary" type="button" disabled>
-                <span
-                  class="spinner-grow spinner-grow-sm"
-                  role="status"
-                  aria-hidden="true"
-                ></span>
-                Please Wait...
-              </button>
-            </div>
-            <div v-else>
-              <button type="button" @click="register()" class="btn btn-primary">
-                Register
-              </button>
-            </div>
-          </div>
+        <div v-else class="alert alert-danger" role="alert">
+          {{ name }}.eth is NOT available!
         </div>
       </div>
+
       <div id="transaction"></div>
     </div>
     <footer class="blog-footer">
@@ -75,22 +68,29 @@
 <script>
 import ABI from './ABI/controller.json';
 import { ethers } from 'ethers';
+import LoadingButton from '@/components/LoadingButton';
 const CONTRACT = '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5';
-const RESOLVER = '0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41';
+// const RESOLVER = '0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41'; //mainnet
+const RESOLVER = '0xf6305c19e814d2a75429Fd637d01F7ee0E77d615'; //Rinkeby
 export default {
   name: 'App',
+  components: {
+    LoadingButton,
+  },
   data() {
     return {
       provider: null,
-      chainId: 1,
+      chainId: 4,
       account: null,
       name: '',
       price: 0,
       days: 30,
       isAvailable: false,
-      wait: false,
-      completed: false,
+      isLoading: false,
+      committed: false,
+      registered: false,
       secret: null,
+      searched: false,
     };
   },
   created: function () {
@@ -112,10 +112,18 @@ export default {
     switchNetwork() {
       window?.ethereum
         ?.request({
-          method: 'wallet_switchEthereumChain',
+          method: 'wallet_addEthereumChain',
           params: [
             {
-              chainId: '0x1',
+              chainId: '0x4',
+              chainName: 'Rinkeby Testnet',
+              nativeCurrency: {
+                name: 'ETH',
+                symbol: 'ETH',
+                decimals: 18,
+              },
+              rpcUrls: ['https://rpc.ankr.com/eth_rinkeby	'],
+              blockExplorerUrls: ['https://rinkeby.etherscan.io/'],
             },
           ],
         })
@@ -132,6 +140,7 @@ export default {
       const contract = new ethers.Contract(CONTRACT, ABI, this.provider);
       this.isAvailable = await contract.available(this.name);
       this.getRentPrice();
+      this.searched = true;
     },
     async commit() {
       this.secret = this.randomSecret();
@@ -152,12 +161,13 @@ export default {
           value: 0,
         })
         .then(async (result) => {
-          this.wait = true;
+          this.isLoading = true;
           await result.wait();
-          setTimeout(function(){
-            this.wait=false;
-            this.completed=true;
-          },30*1000);
+          setTimeout(() => {
+            this.isLoading = false;
+            this.committed = true;
+            this.registered = false;
+          }, 30 * 1000);
           console.log(`${this.account} commitment made`);
         })
         .catch((err) => {
@@ -184,6 +194,9 @@ export default {
         .then(async (result) => {
           await result.wait();
           console.log(`${this.account} register made`);
+          this.isLoading = false;
+          this.committed = true;
+          this.registered = true;
         })
         .catch((err) => {
           console.log(`${err.reason}`);
@@ -196,7 +209,9 @@ export default {
       if (this.isAvailable) {
         const contract = new ethers.Contract(CONTRACT, ABI, this.provider);
         this.price = (
-          (await contract.rentPrice(this.name, this.days * 24 * 60 * 60)) / 1e18
+          ((await contract.rentPrice(this.name, this.days * 24 * 60 * 60)) *
+            2) /
+          1e18
         ).toFixed(6);
       }
     },
---
+++ src/components/LoadingButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-if="!isLoading && !committed && !registered">
+      <button type="button" @click="commit" class="btn btn-primary">
+        Commit
+      </button>
+    </div>
+    <div v-else-if="isLoading && !committed && !registered">
+      <button class="btn btn-secondary" type="button" disabled>
+        <span
+          class="spinner-grow spinner-grow-sm"
+          role="status"
+          aria-hidden="true"
+        ></span>
+        Please Wait...
+      </button>
+    </div>
+    <div v-else-if="!isLoading && committed && !registered">
+      <button type="button" @click="register()" class="btn btn-success">
+        Register
+      </button>
+    </div>
+    <div v-else class="alert alert-success" role="alert">
+      Congraluations! You've succesfully registered <b>{{ name }}.eth</b>!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingButton',
+  props: ['isLoading', 'committed', 'registered', 'commit', 'register', 'name'],
+};
+</script>
